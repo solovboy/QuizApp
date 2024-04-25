c4c35b605b3cd261c1d5b9587d02e2bbf100f03f
Ivan Solovev <iv.soloweow2017@yandex.ru>
2024-04-25 14:44:17 +0300

    Initial Commit

---
+++ QuizApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuizApp
+//
+//  Created by Иван Соловьев on 25.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Иван Соловьев on 25.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
